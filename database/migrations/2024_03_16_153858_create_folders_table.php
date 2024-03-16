86d062356a7f289c49563952590753149b5dd197
Fujisan2002 <nihonsan.0211@icloud.com>
2024-03-16 16:11:16 +0900

    マイグレーションファイルの作成

--- database/migrations/2024_03_16_153858_create_folders_table.php
+++ database/migrations/2024_03_16_153858_create_folders_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('schools', function (Blueprint $table) {
+        Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('school_id')->constrained();
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('schools');
+        Schema::dropIfExists('folders');
     }
 };
