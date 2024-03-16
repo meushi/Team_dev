83aa23633f4784767f6b42dc761c36102bb7107c
Fujisan2002 <nihonsan.0211@icloud.com>
2024-03-16 15:30:23 +0900

    schools

--- database/migrations/2024_03_16_152131_create_schools_table.php
+++ database/migrations/2024_03_16_152131_create_schools_table.php
@@ -13,13 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('schools', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50);
-            $table->string('body', 200);
-            $table->foreignId('category_id')->constrained();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('schools');
     }
 };
