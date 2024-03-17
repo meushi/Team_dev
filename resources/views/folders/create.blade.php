fa6fae815a228ee46bf98563a2edb50f8b5cfbe1
Fujisan2002 <nihonsan.0211@icloud.com>
2024-03-17 17:47:23 +0900

    フォントの色サイズ変更

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -5,23 +5,25 @@
         <title>フォルダ作成画面</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        @vite('resources/css/app.css')
     </head>
     <body>
         <!--<a href='/'>投稿一覧ページへ戻る</a>-->
             <form action="/folders" method="POST">
                 @csrf
+                <p class="title">フォルダ名</p>
                 <div class="title">
                     <input type="text" name="folder[title]" placeholder="タイトル"/>
                 </div>
                 <div class="category">
-                    <h2>Category</h2>
+                    <h2>学校</h2>
                     <select name="folder[school_id]">
                         @foreach($schools as $school)
                             <option value="{{ $school->id }}">{{ $school->name }}</option>
                         @endforeach
                     </select>
                 </div>
-                <button type=“button”></button>
+                <button type=“button”>新規作成</button>
             </form>
     </body>
 </html>
