f2909197dcc97542fbdd6ae6040315c801a76ba0
meushi <taku73baseball@gmail.com>
2024-03-17 17:48:14 +0900

    tailwindインストール

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,11 +5,15 @@
         <title>メッセージ作成画面</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        @vite('resources/css/app.css')
     </head>
     <body>
         <!--<a href='/'>投稿一覧ページへ戻る</a>-->
             <form action={{"/posts/".$folderId}} method="POST">
                 @csrf
+                <p class='posttitle'>
+                    寄せ書き本文
+                </p> 
                 <div class="title">
                     <input type="text" name="post[body]" placeholder="タイトル"/>
                 </div>
